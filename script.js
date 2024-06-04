1638f3ea7d32960c1d94fd71f5c5207764f44157
miibenites <127204728+miibenites@users.noreply.github.com>
2024-06-04 16:24:22 -0300

    att

---
+++ script.js
@@ -0,0 +1,15 @@
+const caixaPrincipal = document.querySelector('.caixa-principal')
+const caixaPerguntas = document.querySelector('.caixa-perguntas')
+const caixaAlternativas = document.querySelector('.caixa-alternativas')
+const caixaResultado = document.querySelector('.caixa-resultado')
+const textoResultado = document.querySelector('.texto-resultado')
+
+const perguntas=[
+    {
+    enunciado: "Pergunta 1" ,
+    alternativas: [ "Arternativa 1" ,
+    "Alternativa 2"
+    ]
+    
+
+}]
